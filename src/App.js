2eafd3ba7a1c5f3aa3acb849c9c9cbfd99d530ff
Chris Boydstun <chrisboydstun@gmail.com>
2019-09-10 17:40:59 -0500

    new committ

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
+  
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   
   const [lionsScore, setLionsScore] = useState(0);
